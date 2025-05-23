8dfed983569d91ef720fe3f2f5cb19bc813b4ec9
Ashish Upadhyay <ashish.ku12@hotmail.com>
2025-05-23 11:06:41 +0530

    Added memory-leak

---
+++ Coding/memory.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void causeMemoryLeak() {
+    int* data = new int[100]; // Allocate memory for 100 integers
+    // ... operations with data ...
+    // Missing delete[] data; statement
+    data[0] = 0;
+}
+
+int main() {
+    causeMemoryLeak();
+    // Memory allocated in causeMemoryLeak is now leaked
+    return 0;
+}
